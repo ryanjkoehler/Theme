9ddfa2407c86781e00e06ed75bfdfe2d98a92162
Luke Watts <sayhello@thisis.la>
2013-07-24 18:00:04 +0100

    Adds comments

--- partials/post/post.php
+++ partials/post/post.php
@@ -9,13 +9,16 @@
 ?><article class="gw gw--rtl stream--article">
 	<div class="col col-five-sixths">
 		<h1 class="h2">
-			<a href="#"><?php the_title(); ?></a>
+			<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
 		</h1>
-		<?php the_content(); ?>
+		<div class="wysiwyg">
+			<?php the_content(); ?>
+		</div>
 	</div><!--
 	--><aside class="col-one-sixth"> 
 		<time datetime="2013-06-17T16:44:32+01:00"><?php echo get_the_date(); ?></time>
-		<p><a href="http://gd3.gdnm.org/2013/06/17/health-propaganda-competition/#respond" title="Comment on Health Propaganda Competition">No comments</a></p>
-		<p><a href="#" class="author"><?php the_author(); ?></a></p> 
+		
+		<?php  echo comments_popup_link( '<span class="leave-reply">' . __( 'Leave a reply', 'socd' ) . '</span>', __( '1 Reply', 'socd' ), __( '% Replies', 'socd' ) ); ?>
+		<p>Posted by<a href="#" class="author"><?php the_author(); ?></a></p> 
 	</aside>	
 </article>
