7768bde8f20466ce0c5ba77d7f1d4f70410e8934
Luke Watts <sayhello@thisis.la>
2013-07-24 18:00:19 +0100

    Adds tabs

--- index.php
+++ index.php
@@ -7,16 +7,27 @@
 get_header(); ?>
 <section class="gw">
 	<header class="header">
-		<h1 class="h1 site--title"><?php bloginfo('name'); ?></h1>
+		<h1 class="h1 site--title"><a href="<?php bloginfo( 'wpurl' ); ?>"><?php bloginfo('name'); ?></a></h1>
 		<h2 class="blog--title"><?php socdinfo('blog_type'); ?></h2>
 	</header><!-- .header -->
 	<section class="col--side col--side__left">
 		<div class="cell colour--blue">
-			<h2 class="h2 h2--ruled">About</h2>
-			<?php bloginfo( 'description' ); ?>
+			<?php if (!dynamic_sidebar( 'left_sidebar' ) ) : ?>
+	
+				<h2 class="h2 h2--ruled">About</h2>
+				<?php bloginfo( 'description' ); ?>
+
+			<?php endif; ?>
 		</div>
 	</section><!-- 
 	--><section class="col--stream stream">
+		<div class="stream--filters">
+			<ul>
+				<li><a href="#" class="s-active">Year 1</a></li><!-- 
+				--><li><a href="#">Year 2</a></li><!-- 
+				--><li><a href="#">Year 3</a></li>
+			</ul>
+		</div>
 		<div class="cell colour--white">
 
 			<div id="posts-container">
@@ -24,6 +35,8 @@ get_header(); ?>
 					<?php get_template_part( 'partials/post/post'); ?>
 				<?php endwhile; ?>
 			</div>
+
+			<?php wp_link_pages(); ?>
 		</div>
 	</section><!--
 	--><aside class="sidebar col--side col--side__right">
