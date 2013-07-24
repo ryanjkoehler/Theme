acddc1a429415986618b7c72c8ea96e26fd8fabc
Luke Watts <sayhello@thisis.la>
2013-07-24 15:53:32 +0100

    Widget styling

--- functions.php
+++ functions.php
@@ -63,6 +63,10 @@ function socd_widets_init() {
 	register_sidebar( array(
 		'name' => 'Blog sidebar',
 		'id'   => 'blog_sidebar',
+		'before_widget' => '<div id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h2 class="h2 h2--ruled">',
+		'after_title'  => '</h2>'
 	) );
 }
 
