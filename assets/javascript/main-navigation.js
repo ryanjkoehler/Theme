5b3397e97a75b9ac9b2103c07d9f6429e7da174d
Oliver Smith <contact@ollyjsmith.com>
2013-07-31 09:28:18 +0100

    add typeahead

--- assets/javascript/main-navigation.js
+++ assets/javascript/main-navigation.js
@@ -5,17 +5,40 @@
 		$ele: $( '.main-navigation-container' ),
 		init: function(){
 			SOCD_menu.init_toggle();
+			SOCD_menu.init_typeahead();
 		},
 		init_toggle: function(){
-			var $menuToggle = SOCD_menu.$ele.find( '.main-navigation-container__mobile-toggle' );
-			console.log( SOCD_menu.$ele );
+			var $menuToggle = $( '.main-navigation-container__mobile-toggle', SOCD_menu.$ele );
 			$menuToggle.click( function( e ){
 				e.preventDefault();
-				$('body').toggleClass( 'state-mobile-menu-visible' );
+				SOCD_states.toggleState( 'state-mobile-menu-visible' );
 			});
+		},
+		init_typeahead: function(){
+			var $searchBox = $( '.site-search__input', SOCD_menu.$ele );			
+			$searchBox.typeahead([
+				{
+					name: 'Blogs',
+					local: [ 'Blog One', 'Blog Two', 'Blog Three', 'Blog Four', 'Blog Five' ]
+				},
+				{
+					name: 'Staff',
+					local: [ 'Luke Watts', 'Tom Lynch', 'Oliver Smith', 'Eva Verhoeven' ]
+				},
+				{
+					name: 'Students',
+					local: [ 'Jonny', 'Jimmy', 'Jamie', 'Jeremy', 'Jerome' ]
+				},
+				{
+					name: 'Courses',
+					local: [ 'Graphic Design: New Media', 'Graphic Design', 'Communication Design', 'Illustration' ]
+				}
+			]);
 		}
 	};
 
 	SOCD_menu.init();
+
+	window.SOCD_menu = SOCD_menu;
 
 })( jQuery );
