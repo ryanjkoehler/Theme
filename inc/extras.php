180d9d6abe91e3fac579f21beaf34dbd869d5f57
Luke Watts <sayhello@thisis.la>
2013-07-31 12:01:21 +0100

    Function for getting all Networks on this network

--- inc/extras.php
+++ inc/extras.php
@@ -22,3 +22,37 @@ function get_post_parent_id ( $post_id = false ) {
 
 	return $post->post_parent ? get_post_parent_id( $post->post_parent ) : $post->ID;
 }
+
+
+function socd_get_networks() {
+	global $wpdb;
+
+
+	$query = "SELECT {$wpdb->site}.*,
+			sm1.meta_value as site_name,
+			sm2.meta_value as site_admins,
+			COUNT({$wpdb->blogs}.blog_id) as blogs,
+			{$wpdb->blogs}.path as blog_path
+		FROM
+			{$wpdb->site}
+		LEFT JOIN
+			{$wpdb->blogs}
+		ON
+			{$wpdb->blogs}.site_id = {$wpdb->site}.id
+		LEFT JOIN
+			{$wpdb->sitemeta} as sm1
+		ON
+			sm1.meta_key = 'site_name' AND
+			sm1.site_id = {$wpdb->site}.id
+		LEFT JOIN
+			{$wpdb->sitemeta} as sm2
+		ON
+			sm2.meta_key = 'site_admins' AND
+			sm2.site_id = {$wpdb->site}.id
+		$searchConditions
+		GROUP BY {$wpdb->site}.id";
+
+	$networks = $wpdb->get_results($query);
+
+	return $networks;
+}
