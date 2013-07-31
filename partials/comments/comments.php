ad5d24cf7938d5bdf8210b7eb355132768d3ddb7
Oliver Smith <contact@ollyjsmith.com>
2013-07-31 18:54:16 +0100

    begin comment styling

--- partials/comments/comments.php
+++ partials/comments/comments.php
@@ -9,20 +9,36 @@ require( get_stylesheet_directory() . '/inc/comments.php' );
 if ( post_password_required() )
 	return;
 ?>
-<section id="comments">
-	<?php if ( have_comments() ) : ?>
-		<h1>
-		<?php
-			printf( _nx( 'One comment', '%1$s comments', get_comments_number(), 'comments title', 'socd' ), number_format_i18n( get_comments_number() ), '<span>' . get_the_title() . '</span>'); ?>
-		</h1>
-	<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
-		<nav id="comment-nav-above" class="navigation-comment" role="navigation">
-			<h1 class="screen-reader-text"><?php _e( 'Comment navigation', 'modernshows' ); ?></h1>
-			<div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', 'modernshows' ) ); ?></div>
-			<div class="nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;', 'modernshows' ) ); ?></div>
-		</nav><!-- #comment-nav-above -->
-	<?php endif; // check for comment navigation ?>
+<section id="comments" class="comments-section">
+	<header class="comments-section--header">
+		<?php if ( have_comments() ) : ?>
+			<h1 class="comments-section--count">
+			<?php
+				printf( _nx( 'One comment', '%1$s comments', get_comments_number(), 'comments title', 'socd' ), number_format_i18n( get_comments_number() ), '<span>' . get_the_title() . '</span>'); 
+			?>
+			<?php 
+				$pages = get_comment_pages_count();	
+				if( $pages > 1 ):
+			?>
+			over 
+			<?php 
+				printf( _nx( 'one page', '%1$s pages', $pages, 'comments title', 'socd' ), number_format_i18n( $pages ) ); 
+			?>
+			<?php 
+				endif; 
+			?>
+			</h1>
+		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
+			<nav id="comment-nav-above" class="comments-section--navigation" role="navigation">			
+				<a href="#" class="nav-previous">Older</a>
+				<a href="#" class="nav-next">Newer</a>
+			</nav><!-- #comment-nav-above -->
+		<?php endif; // check for comment navigation ?>
 
+		<?php if ( ! comments_open() && get_comments_number() ) : ?>
+			<p class="comments-section--nocomments">Comments are closed.</p>
+		<?php endif; ?>
+	</header>
 	<ol class="comments-listing">
 		<?php
 			/* Loop through and list the comments. Tell wp_list_comments()
@@ -32,8 +48,9 @@ if ( post_password_required() )
 			 * See modernshows_comment() in inc/template-tags.php for more.
 			 */
 			wp_list_comments( array(
-				'callback' => 'socd_comment',
-				'format'   => 'html5'
+				'callback' 	=> 'socd_comment',
+				'format'   	=> 'html5',
+				'style' 	=> 'ol'
 			) );
 		?>
 	</ol><!-- .comment-list -->
