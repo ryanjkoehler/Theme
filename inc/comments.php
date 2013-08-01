fabcc9a838349fa43654f1cd5ac937ebd52f9bb6
Oliver Smith <contact@ollyjsmith.com>
2013-08-01 17:01:01 +0100

    pulls in correct comments & nests, further styling

--- inc/comments.php
+++ inc/comments.php
@@ -9,5 +9,5 @@
 function socd_comment($comment, $args, $depth ) {
 	$GLOBALS['comment'] = $comment;
 
-	require_once(get_stylesheet_directory() . '/partials/comments/comment.php');
+	include( get_stylesheet_directory() . '/partials/comments/comment.php' );
 }
--- partials/comments/comment.php
+++ partials/comments/comment.php
@@ -13,38 +13,33 @@ if ( 'pingback' == $comment->comment_type || 'trackback' == $comment->comment_ty
 	<?php else :
 
 	?><li id="comment-<?php comment_ID(); ?>" <?php comment_class( array( empty( $args['has_children'] ) ? '' : 'parent', 'comment') ); ?>>
-		<article id="div-comment-<?php comment_ID(); ?>" class="comment-body palm-cell gw">
-			<footer class="comment-body--comment-meta col">
-				<div class="vcard gw">
-					<div class="col col-one-sixth">
-						<?php echo get_avatar( $comment, 50 ); ?>
-					</div><!--
-				--><div class="col col-five-sixths">
-						<h5 class="h5"><?php echo get_comment_author_link(); ?> </h5>
-						<a href="<?php echo esc_url( get_comment_link( $comment->comment_ID ) ); ?>">
-							<time datetime="<?php comment_time( 'c' ); ?>">
-								<?php echo get_comment_date(); ?> 
-								<?php echo get_comment_time(); ?>
-							</time>
-						</a>
-					</div>
-				</div><!-- .comment-author -->
-
-				<?php if ( '0' == $comment->comment_approved ) : ?>
-					<p class="comment-awaiting-moderation">Your comment is awaiting moderation.</p>
-				<?php endif; ?>
-			</footer><!-- .comment-meta -->
+		<article id="div-comment-<?php comment_ID(); ?>" class="comment-body palm-cell gw">			
 			<div class="comment-body--actions col col-one-sixth">
-				<div class="comment-body-reply h5">
-					<?php comment_reply_link( array_merge( $args, array( 'add_below' => 'div-comment', 'depth' => $depth, 'max_depth' => $args['max_depth'] ) ) ); ?>
-				</div><!-- .reply -->
+				<?php echo get_avatar( $comment, 50 ); ?>				
 				<div class="comment-actions">
 					<?php edit_comment_link( 'Edit', '<span class="edit-link">', '</span>' ); ?>
 				</div><!-- .comment-metadata -->
 			</div><!--
 		--><div class="comment-body--main col col-five-sixths">
+				<footer class="comment-body--comment-meta">
+					<div class="vcard col col-five-sixths">
+							<h5 class="h5"><?php echo get_comment_author_link(); ?> </h5>
+							<a href="<?php echo esc_url( get_comment_link( $comment->comment_ID ) ); ?>">
+								<time datetime="<?php comment_time( 'c' ); ?>">
+									<?php echo get_comment_date(); ?> 
+									<?php echo get_comment_time(); ?>
+								</time>
+							</a>
+					</div><!--
+				--><div class="comment-body--reply h5 col col-one-sixth">
+						<?php comment_reply_link( array_merge( $args, array( 'add_below' => 'div-comment', 'depth' => $depth, 'max_depth' => $args['max_depth'] ) ) ); ?>
+					</div><!-- .reply -->
+					<?php if ( '0' == $comment->comment_approved ) : ?>
+						<p class="comment-awaiting-moderation">Your comment is awaiting moderation.</p>
+					<?php endif; ?>
+				</footer><!-- .comment-meta -->
 				<div class="col comment-body--comment-content wysiwyg">
-					<?php comment_text(); ?>
+					<?php comment_text(  ); ?>
 				</div><!-- .comment-content -->
 			</div>			
 		</article><!-- .comment-body -->
