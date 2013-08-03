9d4cde7534eda78f9ea338512ebe266db25c3b5d
Luke Watts <sayhello@thisis.la>
2013-08-03 09:06:31 +0100

    Adds Staff profile listings

---
+++ page-staff.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Template Name: Staff
+ * 
+ * Archive of the staff
+ * 
+ */
+get_header(); ?>
+<div class="gw">
+	<section>
+		<header class="header">
+			<h1 class="h1 header__title">Staff Profiles</h1>
+		</header>
+		
+		<ul class="h-center col col-two-thirds h-center listing__profiles">
+			<?php
+
+			for ($i=0; $i < 12; $i++) :
+
+				?><li itemscope itemtype="http://schema.org/Person" class="profile col col-one-quarter">
+					<img class="thumb" itemprop="image" src="http://placehold.it/150x150" alt="Staff Name"/>
+					
+					<div class="profile--info">
+						<h1 class="name" itemprop="name">Staff Name</h1>
+						<h2 class="role" itemprop="jobTitle">Role</h2>
+
+						<p>Year of Teaching</p>
+					</div>
+				</li><?php
+
+			endfor; ?>
+		</ul>
+	</section>
+</div>
+<?php get_footer();
