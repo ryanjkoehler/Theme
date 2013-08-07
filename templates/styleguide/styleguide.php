609ff5020916203eba77c4f2122e09b099c40add
Luke Watts <sayhello@thisis.la>
2013-08-07 12:33:03 +0100

    Heading markup patterns

--- templates/styleguide/styleguide.php
+++ templates/styleguide/styleguide.php
@@ -11,6 +11,16 @@ get_header(); ?>
 <h1>Typography</h1>
 <section>
 	<?php for ( $i=1; $i < 6; $i++ ) { printf( '<h%1$d class="h%1$d">Heading %1$d</h%1$d>', $i ); } ?>
+
+  <h2 class="h2">Special Cases</h2>
+
+  <div class="header"><h1 class="h1 site--title">Page Title</h1></div>
+
+  <div class="header">
+    <h1 class="h1 site--title">Page Title</h1>
+    <h2 class="blog--title">with Subheading</h2>
+  </div>
+
 </section>
 <div class="wysiwyg">
 	<p data-fixie class="fixie"></p>
@@ -35,68 +45,73 @@ get_header(); ?>
 </section>
 
 <section>
-<label for="name">Name</label>
-<input class="input_full" type="text" id="name" name="name" required="required">
+  <form action="">
+    
+    <label for="name">Name</label>
+    <input class="input_full" type="text" id="name" name="name" required="required">
 
-<label for="email">Email</label>
-<input class="input_full" type="email" id="email" name="email" required="required">
+    <label for="email">Email</label>
+    <input class="input_full" type="email" id="email" name="email" required="required">
 
-<label for="cc"><abbr title="Courtesy Copy">CC</abbr></label>
-<input type="checkbox" id="cc" name="cc" value="1">
-<label for="cc">Send me a copy of this email</label>
+    <label for="cc"><abbr title="Courtesy Copy">CC</abbr></label>
+    <input type="checkbox" id="cc" name="cc" value="1">
+    <label for="cc">Send me a copy of this email</label>
 
-<label for="tel">Phone</label>
-<input class="input_full" type="tel" id="tel" name="tel" required="required">
+    <label for="tel">Phone</label>
+    <input class="input_full" type="tel" id="tel" name="tel" required="required">
 
-<label for="url">URL</label>
-<input class="input_full" type="text" id="url" name="url" placeholder="http://">
+    <label for="url">URL</label>
+    <input class="input_full" type="text" id="url" name="url" placeholder="http://">
 
-<label for="budget">Budget</label>
-<select class="input_full" id="budget" name="budget">
-  <option value="">Ballpark estimate...</option>
-  <optgroup label="Small">
-    <option value="1k_2k">$1,000 to 2,000</option>
-    <option value="2k_3k">$2,000 to 3,000</option>
-    <option value="3k_4k">$3,000 to 4,000</option>
-    <option value="4k_5k">$4,000 to 5,000</option>
-    <option value="5k_6k">$5,000 to 6,000</option>
-  </optgroup>
-  <optgroup label="Medium">
-    <option value="6k_7k">$6,000 to 7,000</option>
-    <option value="7k_8k">$7,000 to 8,000</option>
-    <option value="8k_9k">$8,000 to 9,000</option>
-    <option value="9k_1k0">$9,000 to 10,000</option>
-    <option value="10k_11k">$10,000 to 11,000</option>
-  </optgroup>
-  <optgroup label="Large">
-    <option value="11k_12k">$11,000 to 12,000</option>
-    <option value="12k_13k">$12,000 to 13,000</option>
-    <option value="13k_14k">$13,000 to 14,000</option>
-    <option value="14k_15k">$14,000 to 15,000</option>
-    <option value="15k_16k">$15,000 to 16,000</option>
-  </optgroup>
-  <optgroup label="Jumbo">
-    <option value="16k_17k">$16,000 to 17,000</option>
-    <option value="17k_18k">$17,000 to 18,000</option>
-    <option value="18k_19k">$18,000 to 19,000</option>
-    <option value="19k_20k">$19,000 to 20,000</option>
-  </optgroup>
-  <optgroup label="Ginormous">
-    <option value="over_20k">over $20,000</option>
-  </optgroup>
-</select>
+    <label for="budget">Budget</label>
+    <select class="input_full" id="budget" name="budget">
+      <option value="">Ballpark estimate...</option>
+      <optgroup label="Small">
+        <option value="1k_2k">$1,000 to 2,000</option>
+        <option value="2k_3k">$2,000 to 3,000</option>
+        <option value="3k_4k">$3,000 to 4,000</option>
+        <option value="4k_5k">$4,000 to 5,000</option>
+        <option value="5k_6k">$5,000 to 6,000</option>
+      </optgroup>
+      <optgroup label="Medium">
+        <option value="6k_7k">$6,000 to 7,000</option>
+        <option value="7k_8k">$7,000 to 8,000</option>
+        <option value="8k_9k">$8,000 to 9,000</option>
+        <option value="9k_1k0">$9,000 to 10,000</option>
+        <option value="10k_11k">$10,000 to 11,000</option>
+      </optgroup>
+      <optgroup label="Large">
+        <option value="11k_12k">$11,000 to 12,000</option>
+        <option value="12k_13k">$12,000 to 13,000</option>
+        <option value="13k_14k">$13,000 to 14,000</option>
+        <option value="14k_15k">$14,000 to 15,000</option>
+        <option value="15k_16k">$15,000 to 16,000</option>
+      </optgroup>
+      <optgroup label="Jumbo">
+        <option value="16k_17k">$16,000 to 17,000</option>
+        <option value="17k_18k">$17,000 to 18,000</option>
+        <option value="18k_19k">$18,000 to 19,000</option>
+        <option value="19k_20k">$19,000 to 20,000</option>
+      </optgroup>
+      <optgroup label="Ginormous">
+        <option value="over_20k">over $20,000</option>
+      </optgroup>
+    </select>
 
-<label for="priority_normal">Priority</label>
-<input type="radio" name="priority" id="priority_urgent" value="Urgent">
-<label for="priority_urgent">Urgent</label>
-<input type="radio" name="priority" id="priority_normal" value="Normal" checked="checked">
-<label for="priority_normal">Normal</label>
-<input type="radio" name="priority" id="priority_low" value="Low">
-<label for="priority_low"><abbr title="Request For Proposal">RFP</abbr> <span class="mute">(low)</span></label>
+    <label for="priority_normal">Priority</label>
+    <input type="radio" name="priority" id="priority_urgent" value="Urgent">
+    <label for="priority_urgent">Urgent</label>
+    <input type="radio" name="priority" id="priority_normal" value="Normal" checked="checked">
+    <label for="priority_normal">Normal</label>
+    <input type="radio" name="priority" id="priority_low" value="Low">
+    <label for="priority_low"><abbr title="Request For Proposal">RFP</abbr> <span class="mute">(low)</span></label>
 
-<label for="description">
-  Project<br>details</label>
-<textarea id="description" name="description" rows="8" required="required"></textarea>
+    <label for="description">
+      Project<br>details</label>
+    <textarea id="description" name="description" rows="8" required="required"></textarea>
+  
+    <input type="submit"/>
+  </form>
 
 </section>
 
