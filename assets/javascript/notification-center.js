dadf77edbcb8aa57b41cd810aa16e2e2d8bc2349
Oliver Smith <contact@ollyjsmith.com>
2013-08-07 18:09:14 +0100

    flexible notification locating and timing

--- assets/javascript/notification-center.js
+++ assets/javascript/notification-center.js
@@ -1,3 +1,25 @@
+/* 
+*	SOCD_Notifications
+*	==================
+*
+* 	Site Wide Notifications
+*
+*	Usage: Notifications.message( message, [ message, ...message ] );
+*
+*	It takes an unlimited number of message arguments, where message is an object, like so:
+
+*		{
+*			text: string containing the message to be displayed
+*			tone: string, the tone of the message, affects styling. choose from: 'positive' | 'negative' | 'ambivalent' (optional, defaults to 'ambivalent')
+*			time: number, number of milliseconds before hiding the message
+*			location: element, the element to associate the message with
+*			position: string, whether to append to the element or place after. Choose from: 'after' | 'append' | 'before' ( optional, defaults to append )
+*		}
+*
+*	It creates html based on the /assets/templates/notifications/message.html template, adding the 'tone' argument as a class.
+*
+*/
+
 ( function( window, $, undefined ){
 	
 	// put our template rendering functions into a quicker to access object, local to this file
@@ -5,81 +27,94 @@
 		message: Templates[ 'notifications--message' ].render.bind( Templates[ 'notifications--message' ] )
 	};
 
-	var notifications = {	
-		$ele: $( '.navigation-notifications' ),
-		$content: $('.notifications-center-display'),
-		notificationTime: 5000,
-		centerMessageCount: 0,
-		timers: [],
-		clearTimers: function(){
-			for( var i = 0; i < notifications.timers.length; i++ ){
-				clearTimeout( notifications.timers[i] );
+	var Notifications = {	
+		_ele: $( '.navigation-notifications' ),
+		_content: $('.notifications-center-display'),
+		_notificationTime: 5000,
+		_centerMessageCount: 0,
+		_timers: [],
+		_clearTimers: function(){
+			for( var i = 0; i < Notifications.timers.length; i++ ){
+				clearTimeout( Notifications.timers[i] );
 			}
 		},
-		handleMessage: function( _opts ){
+		_handleMessage: function( _opts ){
 			var rendered, $rendered;
 			opts = _opts || {};
 			var options = {
-				message: opts.message,
+				text: opts.text,
 				tone: opts.tone || 'ambivalent',
 				time: opts.time || 0,
-				location: opts.location || notifications.$content,
+				location: opts.location || Notifications._content,
 				position: opts.position || 'append'
 			};
 
-			var inNotificationCenter = ( !!opts.location ) ? false : true;
+			var inNotificationCenter = !( !!opts.location );
 
-			if( typeof options.message !== 'string' ){
-				throw new Error( 'SOCD_notifications message objects should have a field, \'message\'' );
+			if( typeof options.text !== 'string' ){
+				throw new Error( 'SOCD_Notifications message objects should have a field, \'text\' which is a string containing the text of the message' );
 				return;
 			}
-			
 
 			rendered = T.message({
-				message: options.message,
+				message: options.text,
 				tone: options.tone 
 			});
+
 			$rendered = $( rendered );
 
-			if( options.position === 'after' ){
-				options.location.after( $rendered );
-			} else {
-				options.location.append( $rendered );
+			switch( options.position ){
+				case 'after':
+					options.location.after( $rendered );
+				break;
+				case 'before':
+					options.location.before( $rendered );
+				break;
+				default:
+					options.location.append( $rendered );
+				break;
 			}
 
 			if( options.time > 0 ){
-				var timer = setTimeout( function(){
-					$rendered.addClass( 'hide' );
-					var tempTimer = setTimeout( function(){
-						$rendered.remove();
-						if( inNotificationCenter ){
-							notifications.centerMessageCount--;
-							if( notifications.centerMessageCount <= 0 ){
-								notifications.$ele.removeClass( 'open' );
-							}
-						}
-					}, 500 );
-					notifications.timers.push( tempTimer );
-				}, options.time );
-				notifications.timers.push( timer );
+				Notifications._removeMessage( $rendered, options.time, inNotificationCenter );
 			}
 
 			if( inNotificationCenter ){
 				// open the notification center
-				notifications.centerMessageCount++;
-				notifications.$ele.addClass( 'open' );
+				Notifications._centerMessageCount++;
+				Notifications._ele.addClass( 'open' );
 			}
 			
 			return $rendered;
 
 		},
-		message: function(  ){
+		_removeMessage: function( $message, when, inNotificationCenter ){
+			var timer = setTimeout( function(){
+				$message.addClass( 'hide' );
+				var tempTimer = setTimeout( function(){
+					$message.remove();
+					if( inNotificationCenter ){
+						//also, if there are no messages left, hide the notification center
+						Notifications._centerMessageCount--;
+						if( Notifications._centerMessageCount <= 0 ){
+							Notifications._ele.removeClass( 'open' );
+						}
+					}
+				}, 500 );
+
+				Notifications._timers.push( tempTimer );
+
+			}, when );
+
+			Notifications._timers.push( timer );
+		},
+		message: function( message ){
 			for( var i = 0; i < arguments.length; i++ ){
-				notifications.handleMessage( arguments[i] );
+				Notifications._handleMessage( arguments[i] );
 			}
 		}
 	};
 
-	window.SOCD_notifications = notifications;	
+	window.SOCD_notifications = Notifications;	
 
 })( window, jQuery );
