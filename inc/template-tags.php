e593fd8a56a876658a2e0d0d5238809f9f3fa0f9
Luke Watts <sayhello@thisis.la>
2013-08-21 18:51:49 +0100

    Filters staff based on current network

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -40,6 +40,19 @@ function is_sketchbook() {
 	return "sketchbook" == $options['blog_type'];
 }
 
+function socd_get_subdomain() {
+	$subdomain = preg_replace( '/https?:\/\//', '', get_bloginfo( 'url' ) );
+
+	$parts = explode('.', $subdomain);
+
+	$subdomain = array_shift( $parts );
+	if ( in_array( $subdomain, array( 'www', 'socd' ) ) ) {
+		return false;
+	}
+
+	return $subdomain;
+}
+
 function socd_posted_on() {
 	printf( __( '<a href="%1$s" title="%2$s" rel="bookmark"><time class="entry-date" datetime="%3$s" pubdate><span class="date">%5$s %6$s</span><span class="time">%4$s</span></time></a><span class="byline">Posted by <span class="author vcard"><a class="url fn n" href="%7$s" title="%8$s" rel="author">%9$s</a></span></span>', 'dj' ),
 		esc_url( get_permalink() ),
@@ -54,9 +67,10 @@ function socd_posted_on() {
 	);
 }
 
-
-
 function socd_network_menu() {
+	if ( !function_exists('switch') ) {
+		
+	}
 	if ( false === ( $output = get_site_transient( 'site__socd_menu' ) ) || false ) {
 		switch_to_blog( 1 );
 		$output = wp_nav_menu( array(
