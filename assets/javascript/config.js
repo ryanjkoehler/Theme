38f33efaa710e1531fa9f1f2737beb18fd861248
Oliver Smith <contact@ollyjsmith.com>
2013-08-23 18:28:54 +0100

    global javascript configuration object, checks that WP passes its values too

---
+++ assets/javascript/config.js
@@ -0,0 +1,8 @@
+if( !window.SOCD ) window.SOCD = {};
+
+if( typeof SOCD.Config === 'undefined' ){ 
+	console.warn( 'Expects to have received SOCD.Config from Wordpress. Things may not work properly...' );
+	SOCD.Config = {};
+}
+
+
