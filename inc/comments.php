3fad3a8e796d9c206df35525041e04e216eddf1b
Oliver Smith <contact@ollyjsmith.com>
2013-08-24 20:51:36 +0100

    define globals as necessary to allow comments callback to function correctly

--- inc/comments.php
+++ inc/comments.php
@@ -11,9 +11,12 @@ function socd_comment_images( ){
 }
 
 function socd_comment( $comment, $args, $depth ) {
+	$GLOBALS['comment'] = $comment;
+	$GLOBALS['depth'] = $depth;
+	$GLOBALS['args'] = $args;
 	socd_template_part( 'comments', 'comment' );
 }	
 
 function socd_comment_end() {
-	echo '</ol>';
+	echo "</ol>";
 }
