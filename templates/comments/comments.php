f0dbf417c7df5ca29a86cb44da0f6e7d96388d19
Luke Watts <luke@thisis.la>
2013-08-28 09:47:02 +0100

    Closes socd-io/wpengine/#57

--- templates/comments/comments.php
+++ templates/comments/comments.php
@@ -6,16 +6,30 @@
 
 require_once get_stylesheet_directory() . '/inc/comments.php';
 
-// Prevent 
+global $wp_query, $post, $wpdb;
+
+// Prevent unauthorised commenting
 if ( post_password_required() ) return; ?>
 <section id="comments" class="comments-section">
 	<?php if ( have_comments() ) : ?>
 	
 	<header class="comments-section--header">	
 		<h1 class="comments-section--count">
-			<?php			
-				// printf( _nx( 'One comment', '%1$s comments', get_comments_number( $post->ID ), 'comments title', 'socd' ), number_format_i18n( get_comments_number( $post->ID ) ), '<span>' . get_the_title() . '</span>'); 
+			<?php
+
+				/**
+				 *  Check comments amount..
+				 * 
+				 * Requires page reload as the $post item is cached, a hacky fix for something which is 
+				 * likely a result of garbled importers.
+				 */
+				if ( count($wp_query->comments) > $post->comment_count ) {
+					$post->comment_count = count( $wp_query->comments );
+					$wpdb->query( $wpdb->prepare("UPDATE $wpdb->posts SET `comment_count` = %d WHERE `id` = %d", count($wp_query->comments), $post->ID ) );
+				}
+				
 				comments_number();
+
 			?>
 			<?php 
 				$pages = get_comment_pages_count();	
@@ -53,6 +67,8 @@ if ( post_password_required() ) return; ?>
 				'format'   		=> 'html5',
 				'style' 		=> 'ol'
 			) );
+
+
 		?>
 	</ol><!-- .comment-list -->
 	<footer class="comments-section--header comments-section--header__footer">
