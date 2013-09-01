547c8e8b29e42bf3f101088b136d9fde0ef16567
Luke Watts <luke@thisis.la>
2013-09-01 15:17:48 +0100

    Closes socd-io/wpengine#64

--- assets/javascript/maps.js
+++ assets/javascript/maps.js
@@ -1,60 +1,85 @@
-console.log(SOCDMapping.center.coordinates.split(','));
-var map = L.map('homepage--map').setView(
-	SOCDMapping.center.coordinates.split(','),
-	8
-);
+/*global SOCDMapping */
+var map;
+var SOCD_MAPTYPE_ID = 'socd_style';
 
-var googleLayer = new L.Google('ROADMAP');
-map.addLayer( googleLayer, true );
-
-// L.tileLayer('http://{s}.tile.cloudmade.com/c1642043366b40a7af2d02fb63680628/997/256/{z}/{x}/{y}.png', {
-// 	minZoom: 6,
-// 	maxZoom: 9,
-// 	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>'
-// }).addTo(map);
-
-// Add our Markers
-if ( SOCDMapping.places.length > 0 ) {
-	console.log(SOCDMapping.places.length, 'Places length' );
-	for (var i = 0, max = SOCDMapping.places.length; i < max; i++) {
-		var place = SOCDMapping.places[i];
-
-		if (!place) continue;
-
-		var latlng = place.locations.coordinates.split(','),
-			popup = L.popup({
-			closeButton: false,
-		})
-		.setLatLng( latlng )
-		.setContent( place.name + ' ' + place.telephone_number );
-
-		L.marker( latlng ).addTo(map).bindPopup( popup );
+function init() {
+	var coordinates = SOCDMapping.center.coordinates.split(',');
+	var mapOptions = {
+    disableDefaultUI: true,
+    scrollwheel: false,
+		zoom: 8,
+    zoomControl: false,
+		center: new google.maps.LatLng( coordinates[0], coordinates[1] ),
+		mapTypeControlOptions:{
+			mapTypeIds: []
+		},
+		mapTypeId: SOCD_MAPTYPE_ID
 	};
+	
+	var map = new google.maps.Map(document.getElementById('homepage--map'), mapOptions);
+
+	var featureOpts = [
+    {
+      "featureType": "transit.line",
+      "stylers": [
+        { "visibility": "off" }
+      ]
+    },{
+      "featureType": "poi",
+      "stylers": [
+        { "visibility": "off" }
+      ]
+    },{
+      "featureType": "road.arterial",
+      "stylers": [
+        { "visibility": "off" }
+      ]
+    },{
+      "featureType": "road.highway",
+      "elementType": "labels",
+      "stylers": [
+        { "visibility": "off" }
+      ]
+    },{
+      "featureType": "administrative",
+      "stylers": [
+        { "visibility": "off" }
+      ]
+    },{
+      "featureType": "water",
+      "elementType": "labels",
+      "stylers": [
+        { "visibility": "off" }
+      ]
+    },{
+      "featureType": "road.highway",
+      "elementType": "geometry",
+      "stylers": [
+        { "visibility": "simplified" }
+      ]
+    }
+];
+
+	map.mapTypes.set(SOCD_MAPTYPE_ID, new google.maps.StyledMapType( featureOpts, {
+		name: "School of Communication Design"
+	} ));
+
+	// Add Markers
+	var markers = SOCDMapping.places;
+
+	if (markers.length) {
+		for (var i = 0; i < markers.length; i++) {
+			var marker = markers[i],
+				coords = marker.locations.coordinates.split(',');
+
+			new google.maps.Marker({
+				position: new google.maps.LatLng( coords[0], coords[1] ),
+				map: map,
+				title: marker.name
+			})
+		};
+	}
+
 }
 
-// Add our map Mask
-/*
-
-var polygon = L.polygon([
-	[ 85, 0 ], // North
-	[ -85 , 0], // South
-	[ -85 , 180], // South/East
-	[ 85 , 180], // North/East		              
-], {
-	color: 'transparent',
-	fillColor: 'lime',
-	fillOpacity: .5
-}).addTo(map);
-
-var polygon = L.polygon([
-	[ 85, 0 ], // North
-	[ -85 , 0], // South
-	[ -85 , -180], // South/East
-	[ 85 , -180], // North/East		              
-], {
-	color: 'transparent',
-	fillColor: 'red',
-	fillOpacity: .5
-}).addTo(map);
-
-*/
+google.maps.event.addDomListener(window, 'load', init);
