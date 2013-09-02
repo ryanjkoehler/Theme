e3403c8377cf7f62cc500a14f9f0b98fbc63b450
Luke Watts <luke@thisis.la>
2013-09-02 09:48:24 +0100

    JS refactor

--- assets/javascript/config.js
+++
@@ -1,8 +0,0 @@
-if( !window.SOCD ) window.SOCD = {};
-
-if( typeof SOCD.Config === 'undefined' ){ 
-	console.warn( 'Expects to have received SOCD.Config from Wordpress. Things may not work properly...' );
-	SOCD.Config = {};
-}
-
-
