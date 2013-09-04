2700c61ac4c0154acb35c40615a0ae73e80f62f1
Oliver Smith <contact@ollyjsmith.com>
2013-09-04 14:04:25 +0100

    main js file to call functinoality from

---
+++ assets/javascript/main.js
@@ -0,0 +1,12 @@
+if( !window.SOCD ){ window.SOCD = {} };
+
+( function( window, $, undefined ){
+	if( $('.listing-filter').length > 0 && $('.listing__students').length > 0 ){
+		console.log( 'students' );
+		SOCD.ListingFilter.init( $('.listing-filter'), $('.listing__students') );
+	} else if( $('.listing-filter').length > 0 &&  $('.listing__profiles').length > 0 ){
+		console.log( 'profiles' );
+		SOCD.ListingFilter.init( $('.listing-filter'), $('.listing__profiles') );
+	}
+
+} )( window, jQuery );
