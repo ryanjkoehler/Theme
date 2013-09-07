91880b4e1df3c57367289478a47693bbdd115e03
Luke Watts <luke@thisis.la>
2013-09-07 13:52:17 +0100

    Adds error catching for yepnope loader

--- assets/javascript/viewport.js
+++ assets/javascript/viewport.js
@@ -1,3 +1,4 @@
+/*globals yepnope */
 (function(SOCD) {
 	var SOCD = window.SOCD;
 
@@ -10,17 +11,20 @@
 		}
 	};
 
-	yepnope([
-		{
-			test: SOCD.Viewport.isBiggerThan(320),
-			yep: [
-				'http://socd.loc/wp-content/themes/socd/assets/javascript/maps.js'
-			],
-			complete: function() {
-				SOCD.Mapping.init();
+	try {
+		yepnope([
+			{
+				test: SOCD.Viewport.isBiggerThan( 380 ),
+				yep: [
+					'http://socd.loc/wp-content/themes/socd/assets/javascript/maps.js'
+				],
+				complete: function() {
+					SOCD.Mapping.init();
+				}
 			}
-		}
-	]);
-
+		]);
+	} catch (e) {
+		console.log("Yepnope not available");
+	}
 
 })(window.SOCD);
