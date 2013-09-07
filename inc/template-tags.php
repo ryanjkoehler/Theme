e0ee9aa9671b2efe86b816a2cabaef0a0d8a1760
Luke Watts <luke@thisis.la>
2013-09-07 13:50:46 +0100

    Force linebreak in author credit

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -85,7 +85,7 @@ function socd_get_subdomain() {
 }
 
 function socd_posted_on() {
-	printf( __( '<a href="%1$s" title="%2$s" rel="bookmark"><time class="entry-date" datetime="%3$s" pubdate><span class="date">%5$s</span><span class="time">%4$s</span></time></a><span class="byline">Authored <span class="author vcard"><a class="url fn n" href="%7$s" title="%8$s" rel="author">%9$s</a></span></span>', 'dj' ),
+	printf( __( '<a href="%1$s" title="%2$s" rel="bookmark"><time class="entry-date" datetime="%3$s" pubdate><span class="date">%5$s</span><span class="time">%4$s</span></time></a><span class="byline">Authored<br/><span class="author vcard"><a class="url fn n" href="%7$s" title="%8$s" rel="author">%9$s</a></span></span>', 'dj' ),
 		esc_url( get_permalink() ),
 		esc_attr( get_the_time() ),
 		esc_attr( get_the_date( 'c' ) ),
