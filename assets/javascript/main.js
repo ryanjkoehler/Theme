f6db4679f1e73d3d1c2400e9c85cd20c51470279
Oliver Smith <contact@ollyjsmith.com>
2013-09-09 17:18:20 +0100

    remove extraneous console.log(s)

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,16 +1,17 @@
 if( !window.SOCD ){ window.SOCD = {} };
 
 ( function( window, $, undefined ){
+
 	if( $('.listing-filter').length > 0 && $('.listing__students').length > 0 ){
-		console.log( 'students' );
 		SOCD.ListingFilter.init( $('.listing-filter'), $('.listing__students') );
 	} else if( $('.listing-filter').length > 0 &&  $('.listing__profiles').length > 0 ){
-		console.log( 'profiles' );
 		SOCD.ListingFilter.init( $('.listing-filter'), $('.listing__profiles') );
 	}
+
 	if( $('.blog-filter').length > 0 ){
 		SOCD.BlogFilter.init( $('.blog-filter'), $('#posts-container'), $('.navigation.stream--paging') );
 	}
+
 	if( $('.search-form').length > 0 ){
 		$('.search-form').each( function(){
 			SOCD.SearchForm( $(this) );
