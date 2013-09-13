08d0645c065a74a7731668fd0f4026fb7cce2414
Oliver Smith <contact@ollyjsmith.com>
2013-09-13 11:24:21 +0100

    closes socd-io/Deployment#135

--- index.php
+++ index.php
@@ -11,18 +11,20 @@ get_header(); ?>
 		<h2 class="blog--title"><?php socdinfo('blog_type'); ?></h2>
 	</header><!-- .header -->
 	<section class="col--side col--side__left">
-		<div class="cell colour--blue">
-			<?php if( is_noticeboard() ): ?>
-				<h2 class="h2 h2--ruled">Filter By</h2>
-				<?php socd_blog_filter(); ?>
-			<?php endif; ?>
-			<?php if ( ! dynamic_sidebar( 'left_sidebar' )  ) : ?>
-				<h2 class="h2 h2--ruled">About</h2>
-				<div class="wysiwyg">
-					<?php bloginfo( 'description' ); ?>
-				</div>
-			<?php endif; ?>
-		</div>
+		<?php if( is_active_sidebar( 'left_sidebar' ) || is_noticeboard() ): ?> 
+			<div class="cell colour--blue">
+				<?php if( is_noticeboard() ): ?>
+					<h2 class="h2 h2--ruled">Filter By</h2>
+					<?php socd_blog_filter(); ?>
+				<?php endif; ?>
+				<?php if ( ! dynamic_sidebar( 'left_sidebar' )  ) : ?>
+					<h2 class="h2 h2--ruled">About</h2>
+					<div class="wysiwyg">
+						<?php bloginfo( 'description' ); ?>
+					</div>
+				<?php endif; ?>
+			</div>
+		<?php endif; ?>
 	</section><!-- 
 	--><section class="col--stream stream">
 		<?php if ( apply_filters( 'socd_is_network', false ) ) do_action( 'socd_year_taxonomy' ); ?>
@@ -36,9 +38,11 @@ get_header(); ?>
 		</div>
 	</section><!--
 	--><aside class="sidebar col--side col--side__right">
-		<div class="cell colour--light-grey">
-			<?php if ( dynamic_sidebar( 'blog_sidebar' ) ) : else : endif; ?>
-		</div><!-- .cell.colour-0grey -->
+		<?php if( is_active_sidebar( 'blog_sidebar' ) ): ?> 
+			<div class="cell colour--light-grey">
+				<?php if ( dynamic_sidebar( 'blog_sidebar' ) ) : else : endif; ?>
+			</div><!-- .cell.colour-0grey -->
+		<?php endif; ?>
 	</aside>
 </section><!-- .gw -->
 
