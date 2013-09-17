711f7748ec17f461abf2c468bd8b0e0c8714fbfb
Luke Watts <luke@thisis.la>
2013-09-17 18:04:10 +0100

    Moves the naming convention about

--- templates/profile/listing.php
+++ templates/profile/listing.php
@@ -6,12 +6,12 @@
 
 global $user;
 
-?><li itemscope itemtype="http://schema.org/Person" class="listing--profile profile__<?php echo is_student() ? "student" : "staff"; ?> col lap--one-third desk--one-fifth" <?php socd_user_match_current_course(); ?> data-year="<?php socd_year_code(); ?>" data-course="<?php socd_course_code(); ?>" data-campus="">
+?><li itemscope itemtype="http://schema.org/Person" class="profile--listing profile__<?php echo is_student() ? "student" : "staff"; ?> col lap--one-third desk--one-fifth" <?php socd_user_match_current_course(); ?> data-year="<?php socd_year_code(); ?>" data-course="<?php socd_course_code(); ?>" data-campus="">
 	<a href="<?php profile_url(); ?>" class="thumbnail col palm--one-third">
 		<?php echo socd_get_profile_thumbnail(); ?>
 	</a>
 	<div class="col palm--two-thirds profile--info">
-		<h1 class="name" itemprop="name"><?php echo $user->display_name; ?></h1>
+		<h1 class="name" itemprop="name"><a href="<?php profile_url(); ?>"><?php echo $user->display_name; ?></a></h1>
 		<?php if ( $role = get_user_meta( $user->ID, 'socd_role', true ) ) : ?>
 		<h2 class="role" itemprop="jobTitle"><?php echo $role; ?></h2>
 		<?php endif; ?>
