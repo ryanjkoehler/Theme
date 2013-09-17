efeff2fd444e87c850ea883934c4855e3834a90f
Oliver Smith <contact@ollyjsmith.com>
2013-09-17 11:17:05 +0100

    not fixing the hover/touch menu thing

--- functions.php
+++ functions.php
@@ -90,7 +90,7 @@ function socd_assets () {
 	wp_enqueue_script( 'socd_main', get_stylesheet_directory_uri() . '/assets/javascript/main.js', array( 'jquery', 'modernizr', 'fitvids', 'socd', 'socd_viewport', 'socd_inline_attach', 'socd_hogan', 'socd_hogan_templates', 'socd_typeahead', 'socd_states', 'socd_main_navigation', 'socd_notification_center', 'socd_comments', 'socd_listing_filters', 'socd_search_form' ), false, true );
 
 	if ( ! is_admin_bar_showing() ) {
-		wp_enqueue_script( 'socd_main_navigation', get_stylesheet_directory_uri() . '/assets/javascript/main-navigation.js', array( 'jquery', 'socd_typeahead', 'socd_states', 'socd_hogan', 'socd_hogan_templates', 'socd_search_form' ), false, true );
+		wp_enqueue_script( 'socd_main_navigation', get_stylesheet_directory_uri() . '/assets/javascript/main-navigation.js', array( 'jquery', 'socd_typeahead', 'socd_states', 'socd_hogan', 'socd_hogan_templates', 'socd_search_form', 'modernizr' ), false, true );
 	}
 
 	// Only used in the registration form
