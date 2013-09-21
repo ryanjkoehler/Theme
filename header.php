6b198461c33bfed4dda7d7e0258fc6299d07e8ec
Luke Watts <luke@thisis.la>
2013-09-21 17:32:44 +0100

    Closes socd-io/Deployment#85

--- header.php
+++ header.php
@@ -19,13 +19,15 @@
 	<?php wp_head(); ?>
 
 	<script>
-	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-	  ga('create', 'UA-43247794-1', 'socd.io');
-	  ga('send', 'pageview');
+		ga('create', 'UA-43247794-1', 'socd.io');
+		<?php if ( $socd_analytics = get_option( 'socd_analytics' ) ) : ?>ga('create', '<?php echo $socd_analytics; ?>', {'name': 'owner' });<?php endif; ?>
+		ga('send', 'pageview');
+		<?php if ( $socd_analytics ) : ?>ga('owner.send', 'pageview'); <?php endif; ?>
 	</script>
 </head>
 <body <?php body_class(); ?>>
