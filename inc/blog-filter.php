19d77c61100c716d8542fdccc80e4db1b477af1e
Luke Watts <luke@thisis.la>
2013-09-24 20:42:38 +0100

    Removes whitespace

--- inc/blog-filter.php
+++ inc/blog-filter.php
@@ -93,5 +93,3 @@ function socd_filter_blog_ajax() {
 
 add_action( 'wp_ajax_socd_blog_filter', 'socd_filter_blog_ajax' );
 add_action( 'wp_ajax_nopriv_socd_blog_filter', 'socd_filter_blog_ajax' );
-
-?>
