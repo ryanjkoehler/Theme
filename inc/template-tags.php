bb1ce509a3de0b54d03ac0bd1392f769ed3fcb4b
Oliver Smith <contact@ollyjsmith.com>
2013-09-27 13:15:24 +0100

    closes socd-io/Deployment#194

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -361,7 +361,8 @@ function socd_filter_years_of_study() {
 
 		if ( $filter !== "student")
 			$output[] = sprintf(
-				'<li><a href="#%1$s">%1$s</a></li>',
+				'<li><a href="#%1$s">%2$s</a></li>',
+				$filter,
 				preg_match('/alumni/', $filter) ? ucfirst( $filter ) : 'Year ' . $filter
 			);
 	}
