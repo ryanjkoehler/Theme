1a84ac314477c287fc7d7b92a3dbb14a41192398
Oliver Smith <contact@ollyjsmith.com>
2013-09-30 10:20:07 +0100

    closes socd-io/Deployment#173

--- search.php
+++ search.php
@@ -13,64 +13,52 @@ get_header(); ?>
 		</h2>
 	</header><!-- .header -->
 	<div class="col desk--two-thirds h-center">
-		<ul class="cell colour--white listing__search-results">
-			<?php while( have_posts() ) : the_post(); ?>
-				<li class="gw listing--result<?php
-					echo ( has_post_thumbnail() ) ? ' listing--result__thumbnail' : '' ;
-					if( get_post_format() ): ?> listing--result__format-<?php echo get_post_format() ?><?php endif; 
-				?>">				
-					<div class="col one-third">
-						<div class="result--heading">
-							<h2 class="result--title h2"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+		<div class="cell colour--white">
+			<ul class="listing__search-results">
+				<?php while( have_posts() ) : the_post(); ?>
+					<li class="gw listing--result<?php
+						echo ( has_post_thumbnail() ) ? ' listing--result__thumbnail' : '' ;
+						if( get_post_format() ): ?> listing--result__format-<?php echo get_post_format() ?><?php endif; 
+					?>">				
+						<div class="col one-third">
+							<div class="result--heading">
+								<h2 class="result--title h2"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+							</div>
+						</div><!--
+					 --><div class="col two-thirds last">
+					 		<div class="result--excerpt wysiwyg">
+					 			<?php 
+						 			if( has_post_format( 'image' ) ){
+						 				$images = get_posts( 
+						 					array(
+							 					'numberposts' => 1,								
+												'post_mime_type' => 'image',
+												'post_parent' => $post->ID,
+												'post_type' => 'attachment'
+							 				),
+							 				ARRAY_N
+						 				);
+						 				if( count( $images ) ){
+											echo '<img src="' . $images[0]->guid . '" />';
+						 				}
+						 			}
+					 			?>
+
+								<?php 
+									if( has_post_format( array( 'quote', 'link' ) ) ){
+										the_content();
+									} else {
+										socd_excerpt();
+									}
+								?>
+							</div>
 						</div>
-					</div><!--
-				 --><div class="col two-thirds last">
-				 		<div class="result--excerpt wysiwyg">
-				 			<?php 
-					 			if( has_post_format( 'image' ) ){
-					 				$images = get_posts( 
-					 					array(
-						 					'numberposts' => 1,								
-											'post_mime_type' => 'image',
-											'post_parent' => $post->ID,
-											'post_type' => 'attachment'
-						 				),
-						 				ARRAY_N
-					 				);
-					 				if( count( $images ) ){
-										echo '<img src="' . $images[0]->guid . '" />';
-					 				}
-					 			}
-				 			?>
-
-							<?php 
-								if( has_post_format( array( 'quote', 'link' ) ) ){
-									the_content();
-								} else {
-									socd_excerpt();
-								}
-							?>
-						</div>
-					</div>
-					
-				</li>
-			<?php endwhile; ?>
-		</ul>
-		<footer class="cell colour--white pagination">
-		<?php
-			//see: http://codex.wordpress.org/Function_Reference/paginate_links#examples
-			global $wp_query;
-
-			$big = 999999999; // need an unlikely integer
-
-			echo paginate_links( array(
-				'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
-				'format' => '?paged=%#%',
-				'current' => max( 1, get_query_var('paged') ),
-				'total' => $wp_query->max_num_pages
-			) );
-		?>
-		</footer>
+						
+					</li>
+				<?php endwhile; ?>
+			</ul>
+			<?php socd_paging_nav(); ?>
+		</div>
 	</div><!-- .col.desk--two-thirds -->
 </section><!-- .gw -->
 <?php get_footer(); ?>
