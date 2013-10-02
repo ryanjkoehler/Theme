fc17cb3f476767ebf397f8ae45bd5e69d5df18a5
Luke Watts <luke@thisis.la>
2013-10-02 19:31:21 +0100

    Refactors filter behaviour

--- assets/javascript/listing-filter.js
+++ assets/javascript/listing-filter.js
@@ -10,35 +10,63 @@ if( !window.SOCD ){ window.SOCD = {} };
 		init: function( _ele, _target ){
 			ListingFilter.$ele = _ele;
 			ListingFilter.$target = _target;
+			ListingFilter.$sections = $('.listing-filter--section', ListingFilter.$el );
 			ListingFilter.$filters = $( '.section-options li a', ListingFilter.$ele );
-			ListingFilter.$items = $( 'li', ListingFilter.$target );
+			ListingFilter.$filterableItems = $( 'li', ListingFilter.$target );
 			ListingFilter.initUI();
 		},
 		initUI: function(){
-			ListingFilter.$filters.after( '<a href="#" class="clear">&times;</a>');
+			// ListingFilter.$filters.after( '<a href="#" class="clear">&times;</a>');
 
-			ListingFilter.$filters.on( 'click', function( e ){
+			ListingFilter.$sections.on( 'click', 'li', function(e) {
 				e.preventDefault();
-				$(this).toggleClass( 'active' );				
+				var $li = $(this);
+				
+				if ( $li.hasClass('s__active') ) {
+					$li.removeClass('s__active');
+				} else {
+					$li.siblings('li').removeClass('s__active');
+					$li.addClass('s__active');
+				}
 				ListingFilter.run();
 			});
 
-			ListingFilter.$filters.siblings( '.clear' ).on( 'click', function( e ){
+			ListingFilter.$ele.on('click', 'footer a', function( e ) {
 				e.preventDefault();
-				$(this).siblings('a').removeClass( 'active' );
-				ListingFilter.run();
-			});
+
+				if ( !ListingFilter.$target.hasClass($(this).data('class')) )
+					ListingFilter.$target.toggleClass('listing__students listing__linear');
+			})
 		},
 		run: function(){
-			var $active = ListingFilter.$items;
-			ListingFilter.$filters.filter('.active').each( function(){
-				var section = $(this).closest( '.listing-filter--section' ).attr('data-section'),
-					by = $(this).attr('href').replace( '#', '' );
-				$active = $active.filter( '[data-' + section + '="' + by + '"]' );
+			var filterString = '';
+
+			ListingFilter.$filterableItems.hide();
+
+			ListingFilter.$sections.each(function() {
+				var $self = $(this),
+					$li = $self.find( '.s__active' ),
+					section = $self.data( 'section' );
+
+				if ( $li.length ) {
+					filterString += ListingFilter.buildFilter( section, $li.find('a').attr('href').replace( '#', '' ) );
+				}
 			});
-			ListingFilter.$items.hide();
-			$active.show();
-		}		
+			console.log(filterString);
+
+			if ( filterString !== "" ) {
+				ListingFilter.$filterableItems.filter(filterString).show();
+			} else {
+				ListingFilter.$filterableItems.show();
+			}
+		},
+		buildFilter: function( section, val ) {
+			return '[data-' + section + '="' + val + '"]';
+		},
+		
+		removeFilter: function( filter ) {
+
+		}	
 	};	
 	
 	window.SOCD.ListingFilter = ListingFilter;
