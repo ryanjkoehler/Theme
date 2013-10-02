538c919abad6fdc9f6fe7beca55e4f7069662613
Luke Watts <luke@thisis.la>
2013-10-02 21:47:32 +0100

    Improves handling of beta block

--- footer.php
+++ footer.php
@@ -12,7 +12,7 @@
 				<h3 class="h4">Site Information</h3>
 				<?php socd_network_footer(); ?>
 			</div><!-- 
-		 --><div class="col push--one-sixth one-third beta--box">
+		 --><div class="col lap--one-half push--desk--one-sixth desk--one-third beta--box">
 				<div class="cell">
 					<h3 class="h3 heading--ruled">Beta Information</h3>
 					<p>The site is currently under <a href="http://make.socd.io">active devlopment</a>. If you should encounter any bugs them please let us know via the <a href="<?php socd_beta_link(); ?>">&beta;</a> tab.</p>
