aa607092c885173aa3bd58122e8de5f99bc166e1
Luke Watts <luke@thisis.la>
2013-10-08 17:08:23 +0100

    Adds support for paginated pages

--- page.php
+++ page.php
@@ -18,9 +18,14 @@
 			<figure class="page--thumbnail">
 				<?php the_post_thumbnail( 'large' ); ?>
 			</figure><?php endif; ?><!-- 
-		 --><div class="col push--desk--one-sixth desk--two-thirds">
+		 --><div class="col lap--two-thirds push--desk--one-sixth desk--two-thirds">
 				<div class="cell colour--white wysiwyg">
 					<?php the_content(); ?>
+					<?php wp_link_pages( array(
+							'before' => '<p></p>',
+							'after'  => '',
+							'next_or_number' => 'next'
+						) ); ?>
 				</div>
 			</div><!-- .col--page --><?php 
 
@@ -30,12 +35,12 @@
 			
 			$subpages = wp_list_pages( array (
 				'child_of' => get_post_parent_id(),
-				'echo'	   => 0
+				'echo'	   => 0,
+				'title_li' => get_the_title( get_post_parent_id() )
 			) );
 
 			if ( $subpages ) :
-
-				?><div class="page--aside">
+				?><div class="col lap--one-third push--desk--one-sixth desk--one-sixth">
 					<div class="cell colour--blue">
 						<?php echo $subpages; ?>
 					</div>
